03d67baff5b4391690779f8576711a0cbf4cf4d0
Thays Peixoto <thabysilva12@gmail.com>
2026-01-12 17:06:26 -0300

    realizada correção de horário de Brasília

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
- 
-  process.env.TZ = '03:00';
+  const app = await NestFactory.create(AppModule); // construção do servidor
+
+  // ajusta o horário do Brasil
+  process.env.TZ = '-03:00';
+
+  //adiciona validação de toda entrada de dados
   app.useGlobalPipes(new ValidationPipe());
+
+  //limita ou libera acesso ao serviço da minha API/back and
   app.enableCors();
 
+  //abertura de porta para receber dados
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
