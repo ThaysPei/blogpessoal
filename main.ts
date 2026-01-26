9bceb07f8d8e8a8558215fbd86fc6ba7f1f6551f
Thays Peixoto <thabysilva12@gmail.com>
2026-01-26 14:15:49 -0300

    adiciona variavel config que recebe document builder

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // construção do servidor
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Generation Brasil","http://www.generationbrasil.online","generation@email.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   // ajusta o horário do Brasil
   process.env.TZ = '-03:00';
